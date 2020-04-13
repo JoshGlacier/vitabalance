61051fc5e16271450fc29f4e89d4d2ecec7c2adf
JoshGlacier <josh_mountain@live.com>
2020-04-12 18:30:41 -0700

    First Commits Basic Button UI

---
+++ comps/button/button.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './button.css';
+//Made the UI
+const CustomButton = ({text, color, onClick}) => <div style={{backgroundColor:color,boxShadow:"1px 1px 5px"+color}}className = "custom_button_box" onClick={onClick}>
+    <div className = "custom_button_box_inner">
+        {text}
+        
+    </div>
+</div>
+
+function ButtonClick() {
+    //Go to next Page
+}
+
+CustomButton.defaultProps = {
+    text:"Let's Go!",
+    color:"#000080",
+    onClick:ButtonClick()
+}
+
+
+export default CustomButton; 
