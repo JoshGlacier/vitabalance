d835b18d1d887d315866c2c773b2f48e9ce4157e
anthonycervi <anthonycervi97gmail.com>
2020-04-18 17:25:14 -0700

    added the header and a just card files

---
+++ comps/header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './header.css';
+
+const Asset_3 = require('./Asset_3.png');
+
+const Header = ({color}) => <div className="header_box">
+    <img src={Asset_3.png} />
+</div>
+
+
+Header.defaultProps = {
+    color:"#043A5B",
+}
+
+export default Header;
