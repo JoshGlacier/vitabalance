24047104bb6531fada83cc9ce10db0d68be1261f
JoshGlacier <josh_mountain@live.com>
2020-04-19 13:21:22 -0700

    Images Work

--- comps/card/index.js
+++ comps/card/index.js
@@ -3,9 +3,19 @@ import './card.css';
 import Header from '../header';
 import Slider from '../slider';
 import CustomButton from '../button';
+import Sleep1 from './Images/Sleep_1.svg';
+import Sleep2 from './Images/Sleep_2.svg';
+import Sleep3 from './Images/Sleep_3.svg';
+import Social1 from './Images/Social _01.svg';
+import Social2 from './Images/Social _02.svg';
+import Social3 from './Images/Social _03.svg';
+import Work1 from './Images/Work_1.svg';
+import Work2 from './Images/Work_2.svg';
+import Work3 from './Images/Work_3.svg';
 
 const Card = () => <div>
  <Header />
+<img src = {Sleep1}></img>
 <Slider />
 <CustomButton />
 </div>
