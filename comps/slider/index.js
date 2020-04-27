416d9c8fe0bd885c9c10ca1b5ab2ee9c6ff0ecd9
Farhaz Ladha <Farhaz@iMac.hitronhub.home>
2020-04-27 12:00:47 -0700

    Sleep Slider WORKS

--- comps/slider/index.js
+++ comps/slider/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './slider.css';
 
-const Slider = () => <div className="slider">
+const Slider = ({onChange}) => <div className="slider">
          <center><p className="bubble"><span id="result">0</span><br/><span id="units">hrs.</span></p></center>
-         <span>&ndash;</span><input type="range" id="slider" name="slider" min="0" max="10" step="1" onChange={showValue}/><span>+</span>
+         <span>&ndash;</span><input type="range" id="slider" name="slider" min="0" max="10" step="1" onChange={()=>{onChange(); showValue();}}/><span>+</span>
 </div>
 
 
--- pages/sleep/index.js
+++ pages/sleep/index.js
@@ -7,6 +7,9 @@ import Card from "../../comps/card";
 import Link from 'next/link';
 import Router from "next/router";
 import Sleep1 from '../../comps/card/Images/Sleep_1.svg';
+import Sleep2 from '../../comps/card/Images/Sleep_2.svg';
+import Sleep3 from '../../comps/card/Images/Sleep_3.svg';
+import './sleep.css'
 
 
 /*function ClickIndex(){
@@ -24,17 +27,37 @@ function ClickIndex(){
 
 const SleepPage = ({}) => { 
     return <div className="custom">
-        
-        <Header />
-<img class = "images" src = {Sleep1}></img>
-<Slider />
+    <Header />
+        <img className="images" src={Sleep1} id="sleep1"></img>
+        <img className="images" src={Sleep2} id="sleep2"></img>
+        <img className="images" src={Sleep3} id="sleep3"></img>
+
+<Slider onChange={sleepResult}/>
+
 <div onClick={ClickIndex} page = "/social">
 <CustomButton page = "/social" OnClick = {ClickIndex}></CustomButton>
 </div>
 </div>
 }
 
+function sleepResult() {
+    console.log(document.querySelector("#slider").value);
+   if(parseInt(document.querySelector("#slider").value) <= 3){
+       document.querySelector("#sleep1").style = 'display: none';
+       document.querySelector("#sleep2").style = 'display: none';
+       document.querySelector("#sleep3").style = 'display: block';
+    } else if (parseInt(document.querySelector("#slider").value) >= 7){
+        document.querySelector("#sleep1").style = 'display: block';
+        document.querySelector("#sleep2").style = 'display: none';
+        document.querySelector("#sleep3").style = 'display: none';
+     } else if (parseInt(document.querySelector("#slider").value) >= 4 || parseInt(document.querySelector("#slider").value) <= 6){
+       document.querySelector("#sleep1").style = 'display: none';
+       document.querySelector("#sleep2").style = 'display: block';
+       document.querySelector("#sleep3").style = 'display: none';
+     }
+}
 
+// Do we need 3 variabes? One for each page to calculate at the end?
 
 
 export default SleepPage;
