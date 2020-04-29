5f73855db2b8fb0ef7a77370124972db868ae1af
JoshGlacier <josh_mountain@live.com>
2020-04-29 12:35:53 -0700

    Button + Video CSS changes

--- comps/button/button.js
+++ comps/button/button.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './button.css';
 //Made the UI
-const CustomButton = ({text, color, onClick}) => <div style={{backgroundColor:color,boxShadow:"1px 1px 5px"+color}}className = "custom_button_box" onClick={onClick}>
+const CustomButton = ({text, color, onClick}) => 
+<div className= "buttondivdivbox">
+<div style={{backgroundColor:color,boxShadow:"1px 1px 5px"+color}}className = "custom_button_box" onClick={onClick}>
     <div className = "custom_button_box_inner">
         {text}
         
     </div>
 </div>
-
+</div>
 
 CustomButton.defaultProps = {
     text:"Let's Go!",
