c8b4bb7f04f37429da3fe222a7b9210833785759
anthonycervi <anthonycervi97gmail.com>
2020-04-29 15:04:45 -0700

    fixed work info page

--- pages/workinfo/index.js
+++ pages/workinfo/index.js
@@ -32,6 +32,40 @@ const WorkInfo = ({}) => {
         7. Figure out your 2 peak hours.
         <br></br>
         8. Pick one task and then do it.
+        <br></br>
+        9. Focus on most important tasks first
+        <br></br>
+        10. Cultivate deep work
+        <br></br>
+        11. Keep a distraction list to stay focused
+        <br></br>
+        12. Use the Eisenhower Matrix to identify long-term priorities
+        <br></br>
+        13. Use the 80/20 rule
+        <br></br>
+        14. Break tasks into smaller pieces
+        <br></br>
+        15.Take breaks
+        <br></br>
+        16. Make fewer decisions
+        <br></br>
+        17. Eliminate inefficient communication
+        <br></br>
+        18. Find repeatable shortcuts
+        <br></br>
+        19. Learn from successes as well as mistakes
+        <br></br>
+        20. Plan for when things go wrong
+        <br></br>
+        21. Work before you get motivated or inspired
+        <br></br>
+        22. Don’t multitask
+        <br></br>
+        23. Fill the tank — recharge
+        <br></br>
+        24. Manage your energy (not just time)
+        <br></br>
+        25. Get better at saying “no”
         </p>
 <div onClick={ClickIndexWork} page = "/sleep">
 <CustomButton OnClick = {ClickIndexWork} text = "Return to Homescreen"></CustomButton>
