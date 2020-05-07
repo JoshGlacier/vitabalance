b32aa78e72001852105d9363cb5f175237124d3b
anthonycervi <anthonycervi97gmail.com>
2020-05-07 12:12:16 -0700

    text hierarchy

--- pages/socialinfo/index.js
+++ pages/socialinfo/index.js
@@ -6,6 +6,8 @@ import Slider from "../../comps/slider"
 import Card from "../../comps/card"
 import Router from "next/router";
 import Social1 from '../../comps/card/Images/Social _01.svg';
+import Social2 from '../../comps/card/Images/Social _02.svg';
+import Social3 from '../../comps/card/Images/Social _03.svg';
 import './socialinfo.css'
 
 
@@ -18,7 +20,63 @@ const SocialInfo = ({}) => {
     return <div className="custom">
  <Header />
 
- <p class = "infoheader">Social Activity: Take The First Step Yourself</p>
+
+ <p class="socialHeader">How to be more social</p>
+
+<span className="imagesContainer">
+<img className="socialImg" src={Social3}></img>
+<img className="socialImg" src={Social2}></img>
+<img className="socialImg" src={Social1}></img>
+</span>
+
+<div className="customdiv">
+
+   <dl className="GeneralInfo">
+   <dt>1. Make a reasonable to-do list. Don't overwhelm yourself.</dt>
+       {/* <dd>Daily sunlight or artificial bright light can improve sleep quality and duration, especially if you have severe sleep issues or insomnia.</dd> */}
+   <dt>2. Set small goals for the tasks.</dt>
+       {/* <dd>Blue light tricks your body into thinking it’s daytime. There are several ways you can reduce blue light exposure in the evening.</dd> */}
+   <dt>3. Focus on one goal at a time.</dt>
+       {/* <dd>Caffeine can significantly worsen sleep quality, especially if you drink large amounts in the late afternoon or evening.</dd> */}
+   <dt>4. Track your time to identify patterns.</dt>
+       {/* <dd>Long daytime naps may impair sleep quality. If you have trouble sleeping at night, stop napping or shorten your naps.</dd> */}
+   <dt>5. Find a method for delegating and following-up.</dt>
+       {/* <dd>Try to get into a regular sleep/wake cycle — especially on the weekends. If possible, try to wake up naturally at a similar time every day.</dd> */}
+   <dt>6. Create a proactive dashboard.</dt>
+       {/* <dd>Avoid alcohol before bed, as it can reduce nighttime melatonin production and lead to disrupted sleep patterns.</dd> */}
+   <dt>7. Figure out your 2 peak hours.</dt>
+       {/* <dd>Optimize your bedroom environment by eliminating external light and noise to get better sleep.</dd> */}
+   
+</dl>
+
+
+<dl className ="GeneralInfo">
+   <dt>8. Pick one task and then do it.</dt>
+       {/* <dd>Test different temperatures to find out which is most comfortable for you. Around 20°C (70°F) is best for most people.</dd> */}
+   <dt>9. Focus on most important tasks first</dt>
+       {/* <dd>Consuming a large meal before bed can lead to poor sleep and hormone disruption. However, certain meals and snacks a few hours before bed may help.</dd> */}
+   <dt>10. Cultivate deep work</dt>
+       {/* <dd>Relaxation techniques before bed, including hot baths and meditation, may help you fall asleep.</dd> */}
+   <dt>11. Keep a distraction list to stay focused</dt>
+       {/* <dd>A warm bath, shower, or foot bath before bed can help you relax and improve your sleep quality.</dd> */}
+   <dt>12. Use the Eisenhower Matrix to identify long-term priorities</dt>
+       {/* <dd>Your bed, mattress, and pillow can greatly affect sleep quality and joint or back pain. Try to buy a high quality bedding — including a mattress — every 5–8 years.</dd> */}
+   <dt>13. Break tasks into smaller pieces</dt>
+       {/* <dd>Regular exercise during daylight hours is one of the best ways to ensure a good night’s sleep.</dd> */}
+   <dt>14. Don’t multitask</dt>
+       {/* <dd>Reduce fluid intake in the late evening and try to use the bathroom right before bed.</dd> */}
+</dl>
+
+
+
+
+</div>
+
+
+
+
+
+ {/* <p class = "infoheader">Social Activity: Take The First Step Yourself</p>
  <div className="customdiv">
 <p className="GeneralInfo"> 
 If you are one of those people who always wait for someone to initiate a conversation, then it is high time you quit the habit and initiated a tête-à-tête yourself. You will be surprised to see the response this change in approach will bring. The number of acquaintances you will make in this way will, by far, outstrip the number that you would have made if you had waited for people to come and approach you. Let’s face it, not everybody has the time to come around talking to all the people that they meet. So, to make sure you do not miss out on the opportunities to meet new people, take matters in your own hands. Not only will this send out a very receptive image of yourself, you might also find a few people with similar interests whom you would otherwise have missed if you had waited for them to come and talk to you.</p>
@@ -29,7 +87,8 @@ Socializing with people is an easy yet intricate affair. It is a lot like walkin
 While there are surely more ways to socialize, the bases for all of those methods lie in these few intricacies. Get them right and you are on your way to mingle like never before.</p>
 
 
-</div>
+</div> */}
+
 <div onClick={ClickIndexSocial} page = "/work">
 <CustomButton OnClick = {ClickIndexSocial} text = "Return to Homescreen"></CustomButton>
 </div>
